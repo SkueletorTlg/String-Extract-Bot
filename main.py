cdb28b4736abe1300fe73b24602e74439f81de0a
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-28 08:20:07 +0530

    Update

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from pyrogram import Client, filters
 
 Bot = Client(
     "String-Extract-Bot",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"]
+    bot_token = os.environ.get("BOT_TOKEN"),
+    api_id = int(os.environ.get("API_ID")),
+    api_hash = os.environ.get("API_HASH")
 )
-
 
 types = [
     "lines",
@@ -20,6 +19,7 @@ types = [
     "urls",
     "domains"
 ]
+
 
 @Bot.on_message(filters.command("start"))
 async def start(bot, update):
